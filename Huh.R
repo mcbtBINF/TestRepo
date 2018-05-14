f488f4c83028202e5e20dfa988e7f907e18d1bb3
mcbtBINF <mbrown67@uncc.edu>
2018-05-14 09:42:13 -0400

    Testing the emacs magit out.

--- Huh.R
+++ Huh.R
@@ -1,5 +1,6 @@
-# Is it is a network drive issue?
-# Making a small edit
-# Making another small edit
-# More edits
-# More edits again
+## Is it is a network drive issue?
+## Making a small edit
+## Making another small edit
+## More edits
+## More edits again
+## Edits coming in from emacs and magit now.
